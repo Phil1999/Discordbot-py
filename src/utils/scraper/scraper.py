03838cc3f421605534f0669a449dc7aaf1651a02
Philip Lee <philiplee1999@gmail.com>
2024-03-08 23:47:18 -0800

    update scraper to include level and class

--- src/utils/scraper/scraper.py
+++ src/utils/scraper/scraper.py
@@ -12,6 +12,8 @@ def get_character_details(username):
     character_details = {
         'name'              : None,
         'image_url'         : None,
+        'level'             : None,
+        'class'             : None,
         'found'             : False
     }
 
@@ -27,16 +29,31 @@ def get_character_details(username):
 
         img_selector = 'img.card-img-top'
         name_selector = 'h3.card-title'
+        level_percentage_selector = 'h5.card-text'
+        class_world_selector = 'p.card-text'
 
         image_tag = soup.select_one(img_selector)
         name_tag = soup.select_one(name_selector)
+        level_percentage_tag = soup.select_one(level_percentage_selector)
+        class_world_tag = soup.select_one(class_world_selector)
 
+        level_class_full_text = level_percentage_tag.text + " " + class_world_tag.text
+        # We expect the string to look something like: "Lv. 287 (14.512%) Shade in Reboot Kronos"
+        text_parts = level_class_full_text.split()
 
         if image_tag:
             character_details['image_url'] = image_tag['src']
             
         if name_tag:
             character_details['name'] = name_tag.text
+
+        if level_percentage_tag:
+            character_level = text_parts[1]
+            character_details['level'] = character_level
+
+        if class_world_tag:
+            character_class = text_parts[3]
+            character_details['class'] = character_class
      
 
     return character_details
