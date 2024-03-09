31e017c6bf908d93cdfa978175303e72a178032b
Philip Lee <philiplee1999@gmail.com>
2024-03-08 21:36:48 -0800

    remove deleting sender message

--- src/bot/responses.py
+++ src/bot/responses.py
@@ -4,23 +4,19 @@ from utils.decorators import delete_invoke_message
 
 def setup_bot(bot):
     @bot.command(name='hello')
-    @delete_invoke_message
     async def hello(ctx):
         await ctx.send(general.hello())
     
     @bot.command(name='guide')
-    @delete_invoke_message
     async def help(ctx):
         await ctx.send(general.help())
     
     @bot.command(name='image')
-    @delete_invoke_message
     async def image(ctx):
         embed = await general.send_image_url()
         await ctx.send(embed=embed)
     
     @bot.command(name='mrx')
-    @delete_invoke_message
     async def mrx(ctx, *usernames: str):
         result, file = await general.send_character_image_url(usernames)
 
