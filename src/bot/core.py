4cc75f0dbf1085133c67d8eb048954924b94c108
phil-rpi3 <philiplee1999@gmail.com>
2024-03-11 03:41:56 +0000

    make bot sync command to guilds

--- src/bot/core.py
+++ src/bot/core.py
@@ -5,11 +5,17 @@ from .responses import setup_bot
 
 def init_discord_bot(token):
     bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
-   
+    
+    
+    
+    guild_ids = [1212500695719223367, 771514804895744021] # List guild IDs here
+
     @bot.event
     async def on_ready():
-        bot.tree.clear_commands(guild=None)
-        await bot.tree.sync()
+        
+        for guild_id in guild_ids:
+            await bot.tree.sync(guild=discord.Object(id=guild_id))
+        
         print(f'{bot.user} has connected to Discord successfully')
 
 
