742fa937fccea003e32a76072505514cb6b40487
mclmcl2 <mclmclf@gmail.com>
2024-03-12 00:45:13 -0400

    fixed directory issue

--- src/bot/commands/general.py
+++ src/bot/commands/general.py
@@ -9,7 +9,7 @@ separator = chr(173) # two line separator
 
 
 async def floppy():
-    file = discord.File(f'assets/floppy.gif', filename = 'floppy.gif')
+    file = discord.File(f'assets/images/floppy.gif', filename = 'floppy.gif')
     return file
 
 
