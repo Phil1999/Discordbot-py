289a6a884a93d298a9d214723fb338ce9c823feb
Philip Lee <philiplee1999@gmail.com>
2024-03-29 23:23:36 -0700

    remove bs4 from dependencies, initial lxml parsing logic implemented

--- src/utils/scraper/scraper.py
+++ src/utils/scraper/scraper.py
@@ -22,14 +22,17 @@ def get_character_details(username):
         if not_found:
             return character_details
         
-        # First, find the 'card-body' div
-        card_div = tree.xpath("//div[@class='card-body text-center'][1]")
-        if not card_div:  # If the card div doesn't exist, the character is not found
+        # Find the 'card-body' div
+        card_div = tree.xpath("//div[contains(@class, 'card-body') and contains(@class, 'text-center')][1]")
+        if not card_div:
             return character_details
         
         # Proceed to extract details from this card div
-        card_div = card_div[0]  # Extract the element from the list
-        
+        card_div = card_div[0]
+       
+
+        # TODO: Refactor so that logic is more robust.
+
         image_tag = card_div.xpath(".//img[@class='card-img-top']/@src")
         name_tag = card_div.xpath(".//h3[@class='card-title text-nowrap']/text()")
         level_class_text = card_div.xpath(".//h5[@class='card-text']/text() | .//p[@class='card-text mb-0']/text()")
@@ -39,14 +42,18 @@ def get_character_details(username):
 
         if image_tag:
             character_details['image_url'] = image_tag[0]
+
         if name_tag:
             character_details['name'] = name_tag[0].strip()
+
         if level_class_text and len(level_class_text) == 2:
             level_percentage, class_world = level_class_text
             full_text = f"{level_percentage} {class_world}"
+            
             match = re.match(r"Lv\. (\d+) \(([\d.]+)%\) (.+?) in (.+)", full_text)
             if match:
                 character_details['level'] = match.group(1)
                 character_details['class'] = match.group(3)
+
 
     return character_details
